ccf7156d9c9f9ebabe13634cf5a9bcbdd99abad1
nonewc <52877410+nonewc@users.noreply.github.com>
2021-12-10 18:02:38 +0900

    review

---
+++ review/DecReview1.java
@@ -0,0 +1,116 @@
+package review;
+
+// import java.util.HashMap;
+
+// import java.util.ArrayList;
+
+// import java.util.Calendar;
+
+public class DecReview1 {
+
+    public static void main(String[] args) {
+        
+        // int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        // String ampm;
+
+        // ampm = (hour < 12) ? "A.M." : "P.M.";
+
+        // System.out.println("지금 시간은 " + hour + "시이고, "+ampm+"입니다.");
+
+        // int a = 1;
+        // long b = 2353245345345L;
+        // double c = 3.14;
+        // float d = 3.1342452356346436F;
+
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(c);
+        // System.out.println(d);
+
+        // String a = "Hello";
+        // String b = "World";
+        // String c = new String("Hello");
+
+        // System.out.println(a.equals(b));
+        // System.out.println(a.equals(c));
+
+        // StringBuffer d0 = new StringBuffer();
+
+        // d0.append("Hello");
+        // d0.append(" ");
+        // d0.append("World");
+
+        // // System.out.println(d0); // 되긴 하는데
+                
+        // String d1 = d0.toString(); // 확실하게 그냥 빼서 써라 (볼 일도 별로 없다.)
+        // System.out.println(d1);
+
+        // String e = "";
+
+        // e += "Hello";
+        // e += " ";
+        // e += "World";
+
+        // System.out.println(e);
+
+        // String[] a = {"a","b","c","d","e"};
+
+        // System.out.println(a[3]);
+        // System.out.println(a.length);
+
+       
+        // ArrayList<String> b = new ArrayList<String>();
+
+        // b.add("A");
+        // b.add("B");
+        // b.add("C");
+
+        // System.out.println(b.size());
+        // System.out.println(b.get(2));
+        // System.out.println(b.remove(2));
+        // System.out.println(b.size());
+
+        // HashMap<String,Integer> maps = new HashMap<String,Integer>();
+
+        // maps.put("height", 167);
+        // maps.put("weight", 65);
+        // maps.put("age", 28);
+
+        // System.out.println(maps.get("height"));
+        // System.out.println(maps.replace("height", 172));
+        // System.out.println(maps.get("height"));
+        // System.out.println(maps.remove("height", 167));
+        // System.out.println(maps.size());
+        // System.out.println(maps.remove("height", 172));
+        // System.out.println(maps.size());
+
+
+     
+        // int x = 50000;
+        // long y = x;
+
+        // long x2 = 5;
+        // int y2 = (int) x2; // casting
+
+        // System.out.println(y2);
+
+    
+
+        // int i1 = -5;
+        // int i2 = +i1;
+        // int i3 = -i1;
+        // int i4 = ++i3
+        // int i5 = i3++
+
+        // System.out.println(i5);
+        // System.out.println(i3);
+
+
+        int i = 5;
+        int j = 2;
+        System.out.println(i / (double)j);
+        // 둘 중 하나는 최소한 실수여야 결과도 실수
+
+    }
+    
+}
