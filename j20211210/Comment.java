e905d681b6f139d17c0f24dbd11cb2c0ca23e3e7
nonewc <52877410+nonewc@users.noreply.github.com>
2021-12-10 14:34:07 +0900

    20211210

---
+++ j20211210/Comment.java
@@ -0,0 +1,22 @@
+// 패키지 선언
+package j20211210;
+
+
+// 클래스명 선언
+public class Comment { // 코드의 우측에도 사용
+    public static void main(String[] args) {
+    
+    //  주석 comment
+
+    //  라인 주석 (한 줄 주석)
+
+    /*
+        블록 주석 (여러 줄 주석)
+    */
+
+    System.out.println("Hello World");
+
+    }
+    
+
+}
