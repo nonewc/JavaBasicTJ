22a6aec206521c80c09f29f78037bf1fa05a3b2f
nonewc <52877410+nonewc@users.noreply.github.com>
2021-12-21 17:51:21 +0900

    20211221

--- JavaInter/src/arrayExam/ArrayExam.java
+++ JavaInter/src/arrayExam/ArrayExam.java
@@ -188,25 +188,105 @@ public class ArrayExam {
 		
 		//순서 뒤집기
 		
-		int[] score = {78,88,98,90,96,67,65,87};
-		int[] temp = new int[score.length];
+//		int[] score = {78,88,98,90,96,67,65,87};
+//		int[] temp = new int[score.length];
+//		
+//		for(int i = 0 ; i < score.length ; i++) {
+//			
+//			temp[i] = score[score.length-1-i];
+//			
+//		}
+//		
+//		score = temp;
+//		
+//		System.out.println(Arrays.toString(score));
+//				
+//		for(int item:score) {
+//			
+//			System.out.print(item+" ");
+//			
+//		}
 		
-		for(int i = 0 ; i < score.length ; i++) {
+		/**/
+		
+		// 배열 : 444657953
+		
+		//0의 갯수 :
+		//1의 갯수 :
+		//2의 갯수 :
+		
+//		int [] numArr = {4,4,4,6,5,7,9,7,5,3};
+//		int [] counter = new int[10];
+//		
+//		for (int i = 0; i < numArr.length ; i++) {
+//			
+//			counter[numArr[i]]++;
+//			// counter[numArr[i]] = counter[numArr[i]] + 1;
+//			
+//		}
+//		
+//		for (int i = 0; i < counter.length; i++) {
+//			
+//			System.out.println(i + "의 갯수 :" + counter[i]);
+//			
+//		}
+
+		/**/
+		
+//		String[] name = new String[3]; // 길이가 3인 String 배열 생성
+//		
+//		name[0] = "Kim";
+//		name[1] = "Song";
+//		name[2] = "Park";
+//		
+//		String[] name2 = {"Kim","Song","Park"};
+//		String[] name3 = new String []{"Kim","Song","Park"};
+//		
+//		
+//		for(String item : name) {
+//			
+//			System.out.print(item + " ");
 			
-			temp[i] = score[score.length-1-i];
+		
+		
+		// char 배열과 String 변환
+		
+//			char[] chArr = {'A','B','C'};
+//			
+//			String str = new String(chArr); // char 배열을 String으로
+//			
+//			System.out.println(str);
+//			
+		
+		
+//			char[] tmp = str.toCharArray(); // 스트링을 char 배열로
+//			
+//			System.out.println(tmp);
 			
+		
+		// src라는 String에 "ABCDE"를 만들어
+		// chArr 이라는 char[]로 변환하여 출력
+		
+		
+		String src= "ABCDE";		
+		
+		for(int i = 0 ; i < src.length(); i++) {
+			
+			char ch = src.charAt(i);
+			System.out.println("src.charAt("+i+"):" + ch);
+					
 		}
 		
-		score = temp;
+//		char[] tmp = src.toCharArray();
+//		
+//		System.out.println(tmp);
 		
-		System.out.println(Arrays.toString(score));
-				
-		for(int item:score) {
-			
-			System.out.print(item+" ");
-			
-		}
 		
+		
+		
+		
+			
+	
 	}
 
 }
---
+++ JavaInter/src/oopExam/variableExam.java
@@ -0,0 +1,74 @@
+package oopExam;
+
+public class variableExam {
+	
+/*
+	선언 위치에 따른 변수의 종류
+		
+		- 클래스 변수; class variable, 인스턴스 변수; instance variable, 지역 변수; local variable
+		- static 붙은 변수는 클래스 변수 static이 붙지 않으면 객체 변수
+		
+			class x{
+			
+					   int iv;	객체 변수 (객체 생성시 생성)
+				static int cv; 클래스 변수(공유 변수, 클래스가 메모리에 올라갈 때 생성)
+				
+				void methodY() {
+				 		int iy = 0; 지역 변수 (메서드 영역의 안쪽에서 선언)
+				 		
+				 }
+			}
+		
+*/
+	
+	public static void main(String[] args) {
+		
+		// 인스턴스 변수, 즉 객체 변수는 객체가 생성 될 때 마다 함께 생성되므로 객체마다 각기 다른 값을 유지할 수 있다.
+		// 반면, 클래스 변수는 모든 객체들의 하나의 저장 공간을 공유한다. 따라서 항상 같은 공통된 값을 갖는다.
+		
+		System.out.println("Card.width = " + Card.width); // static은 객체를 찍지 않고도 접근 가능
+		System.out.println("Card.width = " + Card.height);
+		
+		Card c1 = new Card();
+		c1.kind = "Heart";
+		c1.number = 7;
+		
+		Card c2 = new Card();
+		c2.kind = "Spade";
+		c2.number = 4; // 따로 따로 관리함
+		
+		System.out.println("c1은 " + c1.kind + ", "+ c1.number);
+		System.out.println("c2는 " + c2.kind + ", "+ c2.number);
+		
+		c1.width = 50;	// c1을 수정했지만
+		c1.height = 150;
+		
+		System.out.println("- 클래스 변수 출력 - ");
+		System.out.println("Card.width = " + Card.width);  // Card.width도 공유가 되어버림
+		System.out.println("Card.width = " + Card.height);
+						
+		System.out.println("- c2 변수 출력 - ");
+		System.out.println("c2.width = " + c2.width);  
+		System.out.println("c2.width = " + c2.height);
+		
+		System.out.println("- c1 변수 출력 - ");
+		System.out.println("c1.width = " + c1.width);  
+		System.out.println("c1.width = " + c1.height);
+	}
+	
+	
+	
+	
+}
+
+	class Card{
+		
+		String kind; // 객체 변수
+		int number; // 객체 변수
+		static int width = 100; // 클래스 변수
+		static int height = 250; // 클래스 변수
+		
+	}
+
+
+
